7004cd3fc2cf5c6a999b54a001f27f1d3bad619c
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-09-10 02:37:35 +0100

    add gitignore

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-
+// git project path to run in static server
 app.use(express.static(path.join(__dirname,'public')))
 
 const PORT = 3000 || process.env.PORT;
