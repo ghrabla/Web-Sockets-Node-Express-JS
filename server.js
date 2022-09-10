29fefb66425d68a9dc60936d1046903501adcbff
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-09-10 02:54:40 +0100

    add utils

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
 // git project path to run in static server
 app.use(express.static(path.join(__dirname,'public')))
 
+io.on('connection', socket => {
+    console.log('new ws connection ...')
+})
+
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT,() => console.log(`server is running in port ${PORT}`))
+server.listen(PORT,() => console.log(`server is running in port ${PORT}`))
