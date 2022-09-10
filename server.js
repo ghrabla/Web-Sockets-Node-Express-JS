1f098ca32e92efdb4da2d67efb75b7c2c1388c30
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-09-10 02:35:13 +0100

    setup the server on public folser

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')))
+
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT,() => console.log(`server is running in port ${PORT}`))
